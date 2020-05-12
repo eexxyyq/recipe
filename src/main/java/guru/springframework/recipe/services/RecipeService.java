831013e03b37b138fad05eeec9bf244ad96b5c66
eexxyyq <eexxyyq@gmail.com>
2020-05-13 01:13:20 +0300

    add showing recipe by id

--- src/main/java/guru/springframework/recipe/services/RecipeService.java
+++ src/main/java/guru/springframework/recipe/services/RecipeService.java
@@ -6,4 +6,6 @@ import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getAllRecipes();
+
+    Recipe getRecipeById(Long id);
 }
--- src/main/java/guru/springframework/recipe/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipe/services/RecipeServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -20,5 +21,14 @@ public class RecipeServiceImpl implements RecipeService{
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().forEach(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe getRecipeById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if (recipeOptional.isEmpty()) {
+            throw new RuntimeException("This is wrong id");
+        }
+        return recipeOptional.get();
     }
 }
